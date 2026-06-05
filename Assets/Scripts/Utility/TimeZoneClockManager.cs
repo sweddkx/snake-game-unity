67ef6e033e1490ffcccbb447c89e0d2457530845
sweddkx <luxarcadia70186@gmail.com>
2026-06-05 22:31:28 +0800

    Add TimeZoneClockManager script for displaying multiple time zones

---
+++ Assets/Scripts/Utility/TimeZoneClockManager.cs
@@ -0,0 +1,142 @@
+using UnityEngine;
+using TMPro;
+using System;
+using System.Collections.Generic;
+
+public class TimeZoneClockManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class TimeZoneDisplay
+    {
+        public string timeZoneName;
+        public string timeZoneId;
+        public TextMeshProUGUI timeText;
+        public TextMeshProUGUI dateText;
+    }
+    
+    [SerializeField] private List<TimeZoneDisplay> timeZones = new List<TimeZoneDisplay>();
+    [SerializeField] private TextMeshProUGUI localTimeText;
+    [SerializeField] private TextMeshProUGUI utcTimeText;
+    [SerializeField] private float updateInterval = 0.1f;
+    
+    private float updateTimer = 0f;
+    
+    private void Start()
+    {
+        // Initialize with some common time zones
+        if (timeZones.Count == 0)
+        {
+            InitializeDefaultTimeZones();
+        }
+    }
+    
+    private void Update()
+    {
+        updateTimer += Time.deltaTime;
+        
+        if (updateTimer >= updateInterval)
+        {
+            UpdateAllClocks();
+            updateTimer = 0f;
+        }
+    }
+    
+    private void UpdateAllClocks()
+    {
+        DateTime utcNow = DateTime.UtcNow;
+        
+        // Update UTC time
+        if (utcTimeText)
+        {
+            utcTimeText.text = $"UTC\n{utcNow:HH:mm:ss}\n{utcNow:dddd, MMMM dd, yyyy}";
+        }
+        
+        // Update local time
+        if (localTimeText)
+        {
+            DateTime localTime = DateTime.Now;
+            localTimeText.text = $"Local Time\n{localTime:HH:mm:ss}\n{localTime:dddd, MMMM dd, yyyy}";
+        }
+        
+        // Update each time zone
+        foreach (TimeZoneDisplay tzDisplay in timeZones)
+        {
+            if (tzDisplay.timeText == null) continue;
+            
+            try
+            {
+                TimeZoneInfo tzInfo = TimeZoneInfo.FindSystemTimeZoneById(tzDisplay.timeZoneId);
+                DateTime tzTime = TimeZoneInfo.ConvertTime(utcNow, tzInfo);
+                
+                tzDisplay.timeText.text = $"{tzDisplay.timeZoneName}\n{tzTime:HH:mm:ss}";
+                
+                if (tzDisplay.dateText)
+                {
+                    tzDisplay.dateText.text = $"{tzTime:dddd, MMMM dd, yyyy}";
+                }
+            }
+            catch (TimeZoneNotFoundException)
+            {
+                if (tzDisplay.timeText)
+                    tzDisplay.timeText.text = $"{tzDisplay.timeZoneName}\n[TZ Not Found]";
+            }
+        }
+    }
+    
+    private void InitializeDefaultTimeZones()
+    {
+        // Default time zones to display
+        string[] defaultZones = new string[]
+        {
+            "Pacific Standard Time",     // Los Angeles
+            "Mountain Standard Time",    // Denver
+            "Central Standard Time",     // Chicago
+            "Eastern Standard Time",     // New York
+            "GMT Standard Time",         // London
+            "Central European Standard Time", // Paris/Berlin
+            "India Standard Time",       // India
+            "China Standard Time",       // Shanghai
+            "Tokyo Standard Time",       // Tokyo
+            "Australia/Sydney"           // Sydney
+        };
+        
+        Debug.Log("Initialize with default time zones from code to see them in inspector");
+    }
+    
+    // Public method to add a custom time zone
+    public void AddTimeZone(string timeZoneName, string timeZoneId, TextMeshProUGUI timeText, TextMeshProUGUI dateText = null)
+    {
+        TimeZoneDisplay newTz = new TimeZoneDisplay
+        {
+            timeZoneName = timeZoneName,
+            timeZoneId = timeZoneId,
+            timeText = timeText,
+            dateText = dateText
+        };
+        
+        timeZones.Add(newTz);
+    }
+    
+    // Remove a time zone display
+    public void RemoveTimeZone(int index)
+    {
+        if (index >= 0 && index < timeZones.Count)
+        {
+            timeZones.RemoveAt(index);
+        }
+    }
+    
+    // Get all available time zones
+    public static string[] GetAvailableTimeZones()
+    {
+        TimeZoneInfo[] zones = TimeZoneInfo.GetSystemTimeZones();
+        string[] zoneIds = new string[zones.Length];
+        
+        for (int i = 0; i < zones.Length; i++)
+        {
+            zoneIds[i] = zones[i].Id;
+        }
+        
+        return zoneIds;
+    }
+}
